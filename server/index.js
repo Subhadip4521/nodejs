a82e2cb47ce1b4f0df479ce3305f779c5a217758
Subhadip Das <subhadipdas4521@gmail.com>
2024-06-16 22:21:29 +0530

    update 160624

--- server/index.js
+++ server/index.js
@@ -1,11 +1,30 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const myServer = http.createServer((req, res) => {
-  const log = `${Date.now()} : ${req.url} : New request received\n`;
+  if (req.url === "/favicon.ico") {
+    return res.end();
+  }
+  const log = `${Date.now()} : ${req.method} : ${
+    req.url
+    } : New request received\n`;
+  
+  const myUrl = url.parse(req.url, true);
+
   fs.appendFile("./log.txt", log, (err, data) => {
-    res.end("Hello from server");
+    switch (myUrl.pathname) {
+      case "/":
+        res.end("Hello");
+        break;
+      case "/about":
+        res.end("Hello about");
+        break;
+      default:
+        res.end("404 not found");
+        break;
+    }
   });
 });
 
-myServer.listen(8000, ()=> console.log("Server listening on PORT:8000"));
+myServer.listen(8000, () => console.log("Server listening on PORT:8000"));
